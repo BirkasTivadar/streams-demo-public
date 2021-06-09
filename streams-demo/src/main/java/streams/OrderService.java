5f5f6d8cedb24e959e5ea89d508082d374db8c8d
BirkasTivadar <birkastivadar@gmail.com>
2021-06-09 06:38:25 +0200

    week01d02 feladat

--- streams-demo/src/main/java/streams/OrderService.java
+++ streams-demo/src/main/java/streams/OrderService.java
@@ -22,11 +22,20 @@ public class OrderService {
     }
 
     public List<Product> productsOverAmountPrice(int amount) {
-        orders.stream().
+       return orders.stream().
                 flatMap(o -> o.getProducts().stream())
                 .filter(e -> e.getPrice() > amount)
                 .distinct() // ha csak egyszer akarjuk
                 .collect(Collectors.toList());
     }
+
+//    Órán megoldott feladatok:
+//    Határozd meg a rendelések darabszámát egy paraméterként kapott státusz alapján
+//    Gyűjtsd össze azokat a rendeléseket, amelyekben van egy paraméterként kapott kategóriájú termék
+//    Gyűjtsd össze azokat a termékeket a rendelések közül, amelyeknek egy paraméteról kapott összegnél magasabb az áruk
+//    További gyakorló feladatok:
+//    Írj egy metódust ami paraméterként vár két dátumot, és adjuk vissza a két dátum közötti árbevételt, vagyis a két dátum közötti rendelések termékeinek az összértékét!
+//    Keressünk meg egy terméket a neve alapján, amit paraméterként lehet megadni.
+//    Adjuk vissza azt a rendelést, ami a legdrágább terméket tartalmazza. Ha több ilyen van bármelyiket!
 
 }
